8dc0cff2f75145c6eeccc6710b86a9a349c7c353
angela <angelaga>
2026-07-31 11:36:03 -0500

    configuracion de variables de entorno y parametro

--- app.js
+++ app.js
@@ -2,13 +2,44 @@ import express from 'express';
 //leer el archivo .env
 import {configDotenv} from "dotenv"
 configDotenv()
+
+
 const app = express();
-const port = process.env.PUERTO || 3030;
+const puerto = process.env.PUERTO || 3030
 
 app.get('/', (__, res) => {
   res.send('Aprendiendo express,ficha 3407181, ADSO EN EL SENA 23, 31 de julio');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en el puerto: ${port}`); http:localhost:3030
+
+app.get("/Otraruta", (req, res)=>{
+  //usando template string
+  res.send(`<h1>Otra ejemplo de ruta</h1>
+    <h2>End point con res.send</h2>`)
+  
+})
+
+app.get("/ruta2", (req, res) =>{
+  res.json({"Nombre":"Angela", "Apellido": "Gaviria", "Cargo": "Aprendiz"})
+})
+
+app.get("/ruta3/:aprendiz/:otrodato", (req, res) =>{
+  const dato_aprendiz = req.params.aprendiz
+  const otro_dato = req.params.otrodato
+  res.json({"Nombre": dato_aprendiz, "Otro": otro_dato})
+})
+
+app.get("/ruta4", (req, res) =>{
+  //capturar el parametro de consulta query 
+  const orden = req.query.orden || "sin ordenar"
+  const pagina = req.query.pagina || 1
+  res.send(`<h1>listado Aprendices</h1>
+    <p> El listado esta en orden ${orden}</p>
+    <p>Pagina: ${pagina}</p>
+    `)
+})
+
+
+app.listen(puerto, function (){
+  console.log(`SERVIDOR  ${puerto} http://localhost:3030/`);
 });
