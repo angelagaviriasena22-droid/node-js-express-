82f092529832b60d62217ab0713741e499d7ca0f
angela <angelaga>
2026-07-31 10:30:44 -0500

    aplicacion configurando las variables de entorno

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
+//leer el archivo .env
+import {configDotenv} from "dotenv"
+configDotenv()
 const app = express();
-const port = process.env.PUERTO || 3000;
+const port = process.env.PUERTO || 3030;
 
 app.get('/', (__, res) => {
-  res.send('Aprendiendo express,ficha 3407181, ADSO EN EL SENA 23');
+  res.send('Aprendiendo express,ficha 3407181, ADSO EN EL SENA 23, 31 de julio');
 });
 
 app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+  console.log(`Servidor en funcionamiento en el puerto: ${port}`); http:localhost:3030
 });
